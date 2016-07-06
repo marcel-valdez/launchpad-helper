342f42a818f194bbcff615b2bdbf04fba93502a7
Marcel Valdez <marcelvaldez@google.com>
2016-07-06 09:41:00 -0700

    installer: Add capacity to append line to bashrc and to create HOME/bin if it does not exist

--- install.sh
+++ install.sh
@@ -2,8 +2,20 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ ! -d $HOME/bin ]; then
-  mkdir $HOME/bin
+if [ ! -d "$HOME/bin" ]; then
+  echo "No $HOME/bin directory exists, do you wish to create one? [yY]"
+  read __answer
+  if [ "$__answer" == "y" ] || [ "$__answer" == "Y" ]; then
+    mkdir $HOME/bin
+    echo "Add PATH entry for $HOME/bin in $PATH"
+    echo 'if [ -d $HOME/bin ]; then'
+    echo '  export PATH=$PATH:"$HOME/bin"' >> "$HOME/.bashrc"
+    echo 'fi'
+  fi
+fi
+
+if [ ! -d "$HOME/bin" ]; then
+  echo "There is no $HOME/bin directory, cannot continue with installation"
 fi
 
 echo "Copying files to $HOME/bin"
@@ -12,5 +24,18 @@ cp --verbose $SCRIPT_DIR/__latest_version.py $HOME/bin
 cp --verbose $SCRIPT_DIR/__check_version.py $HOME/bin
 cp --verbose $SCRIPT_DIR/verify-packages $HOME/bin
 
+
+
 echo "Export $HOME/dir to your PATH variable in order to have the"
-echo "command line tools available in a terminal"
+echo "command line tools available in a terminal."
+
+echo "Do you wish to check for your package versions every computer restart? [yY]"
+read __answer
+
+if [ "$__answer" == "y" ] || [ "$__answer" == "Y" ]; then
+  echo "verify-packages --use-cache" >> "$HOME/.bashrc"
+else
+  echo "In order to have the tool check versions every time you restart"
+  echo "your computer, you need to add this line to your .bashrc:"
+  echo "verify-packages --use-cache"
+fi

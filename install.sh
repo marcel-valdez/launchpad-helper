c56f5ae78f236edba56b469aa1e964b4acb8db58
Marcel Valdez <marcelvaldez@google.com>
2016-08-01 21:43:36 -0700

    Add update-package to install/uninstall script

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ cp --verbose $SCRIPT_DIR/check-all-versions $HOME/bin
 cp --verbose $SCRIPT_DIR/__latest_version.py $HOME/bin
 cp --verbose $SCRIPT_DIR/__check_version.py $HOME/bin
 cp --verbose $SCRIPT_DIR/verify-packages $HOME/bin
+cp --verbose $SCRIPT_DIR/update-package $HOME/bin
 
 
 
